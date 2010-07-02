b29e9b23753789c5c1cf909ba0af70f694af2a09
Grant Klopper <gklopper@gnm41487.(none)>
2010-07-02 14:39:04 +0100

    There was a bug - now using urlfetchservice

--- src/com/gu/upload/proxy/ProxyServlet.java
+++ src/com/gu/upload/proxy/ProxyServlet.java
@@ -1,17 +1,18 @@
 package com.gu.upload.proxy;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 
 @SuppressWarnings("serial")
 public class ProxyServlet extends HttpServlet {
@@ -21,17 +22,21 @@ public class ProxyServlet extends HttpServlet {
 		
 		String url = request.getParameter("url");
 		
-		URLConnection connection = new URL(url).openConnection();
-		response.setHeader("Content-Type", connection.getContentType());
+		HTTPResponse sourceResponse = URLFetchServiceFactory.getURLFetchService().fetch(new URL(url));
+		
+		byte[] contents = sourceResponse.getContent();
+		
+		for (HTTPHeader header : sourceResponse.getHeaders()) {
+			String name = header.getName().toLowerCase();
+			if (name.equals("content-type")) {
+				response.setHeader("Content-Type", header.getValue());
+			}
+		}
+		
 		response.setHeader("Cache-Control", "max-age=86000");
 		
-		InputStream in = new BufferedInputStream(connection.getInputStream());
 		OutputStream out = new BufferedOutputStream(response.getOutputStream());
-		int x;
-		
-		while ((x = in.read()) != -1) {
-			out.write(x);
-		}
+		out.write(contents);
 		out.flush();
 	}
 }
