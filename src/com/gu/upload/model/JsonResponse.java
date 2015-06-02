dadc7748875cddd312a97537b8305b0adde7d181
Justin Pinner <justinpinner@gmail.com>
2015-06-02 12:24:38 +0100

    Deserialise json into correct data type. Add more logging.

--- src/com/gu/upload/model/JsonResponse.java
+++ src/com/gu/upload/model/JsonResponse.java
@@ -1,19 +1,25 @@
 package com.gu.upload.model;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 public class JsonResponse {
 
+    @SerializedName("files")
     private List<StaticFile> files;
+
+    public JsonResponse() {
+        // no-args constructor
+    }
 
     public JsonResponse(List<StaticFile> staticFiles) {
         this.files = staticFiles;
     }
 
-    public List<StaticFile> getFiles() {
-        return files;
+    public final List<StaticFile> getFiles() {
+        return this.files;
     }
 
     public String toJson() {
--- src/com/gu/upload/tools/BlobImportServlet.java
+++ src/com/gu/upload/tools/BlobImportServlet.java
@@ -7,6 +7,7 @@ import com.google.appengine.api.files.AppEngineFile;
 import com.google.appengine.api.files.FileService;
 import com.google.appengine.api.files.FileServiceFactory;
 import com.google.appengine.api.files.FileWriteChannel;
+import com.google.gson.reflect.TypeToken;
 import com.gu.upload.model.JsonResponse;
 import com.gu.upload.model.StaticFile;
 import com.gu.upload.tools.util.HttpRequest;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,26 +50,37 @@ public class BlobImportServlet extends HttpServlet {
                 String url = "http://static-host.appspot.com/staticfile/json/" + localFile.getPath();
                 String json = HttpRequest.get(url).contentType("application/json").body();
                 log("Response was: " + json);
-                JsonResponse jsonResponse = gson.fromJson(json, JsonResponse.class);
 
+                Type responseType = new TypeToken<JsonResponse>(){}.getType();
+                JsonResponse jsonResponse = gson.fromJson(json, responseType);
                 List<StaticFile> remoteFiles = jsonResponse.getFiles();
 
                 for (StaticFile remoteFile : remoteFiles) {
+                    log("Checking remote file: " + remoteFile.getPath());
                     if (remoteFile.getPath().equals(localFile.getPath())) {
+                        log("Processing remote file: " + remoteFile.getPath());
                         // Create a new Blob file with expected mime-type
+                        log("  createNewBlobFile");
                         AppEngineFile newLocalFile = fileService.createNewBlobFile(localFile.getType());
                         // Open a channel to write to it
                         boolean lock = true;
+                        log("  openWriteChannel");
                         FileWriteChannel writeChannel = fileService.openWriteChannel(newLocalFile, lock);
                         // Write the blob data to the channel directly
+                        log("  writeChannel.write");
                         writeChannel.write(ByteBuffer.wrap(remoteFile.getData().getBytes()));
                         // And finalize
+                        log("  writeChannel.closeFinally");
                         writeChannel.closeFinally();
                         // Get the new file's BlobKey
+                        log("  fileService.getBlobKey");
                         BlobKey blobKey = fileService.getBlobKey(newLocalFile);
                         // Update the local StaticFile's blobkey
+                        log(" localFile.setBlobKey");
                         localFile.setBlobKey(blobKey.getKeyString());
+                        log("  localFile.update");
                         localFile.update();
+                        log("Done");
                     }
                 }
 
